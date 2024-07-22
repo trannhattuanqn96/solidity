74bc67855fc3ee43a1e7fc080733d8e70cb6ca0d
Tuan Tran <trannhattuanqn96@gmail.com>
2024-07-22 21:01:19 +0700

    update

--- contracts/Calculator.sol
+++ contracts/Calculator.sol
@@ -1,12 +1,16 @@
 //SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.19;
+import './Hello.sol';
 
-contract Calculator {
+contract Calculator  is Hello {
     // khai báo 1 biến result với loại là uint256 và giá trị là 0
     uint256 result = 0;
 
     // hàm có chức năng cộng vào result được khai báo ở trên
+    constructor(uint _a) {
+        result = _a;
+    }
 
     function add(uint256 num) public {
         result += num;
